c7987d0eebbab974c4cb5f0c299e171a94b3c6be
Brian Li <bl195@duke.edu>
2020-09-11 12:36:08 -0400

    added landing and register

--- image-classifier-app/src/app/components/landing/landing.component.ts
+++ image-classifier-app/src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  router: any;
+  valid: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+  onRegister(){
+    alert("register")
+    this.router.navigateURL(['/register'])
+  }
+
+  onLogin(){
+    const email = <HTMLInputElement>document.getElementById("email").value;
+    alert(email)
+    this.http.post<any>("http://localhost:8000/",email)
+    .subscribe((
+      data) => {
+        this.valid = data.valid;
+      },
+      err => alert("ERROR:"+ err.message)
+    );
+    if(this.valid=="true"){
+      this.router.navigateURL(['/home'])
+    }
+  }
+
 
 }
